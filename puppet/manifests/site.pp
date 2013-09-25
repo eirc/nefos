6c8c125b8e9b6806ffd65395ddbdaec7a09ddb88
Eric Cohen <eirc.eirc@gmail.com>
2013-09-26 01:20:28 +0300

    All machines apt-get update before installing any package.

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,1 +1,5 @@
 import 'nodes/*.pp'
+
+# Require an apt-get update for all package declarations
+exec { 'apt-get update': path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' }
+Exec['apt-get update'] -> Package <| |>

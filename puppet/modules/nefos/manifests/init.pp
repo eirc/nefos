865242072d153a5c619331e3bc3d44e5fae5da44
Eric Cohen <eirc.eirc@gmail.com>
2013-09-28 00:45:19 +0300

    Added various useful common packages to all machines.

--- puppet/modules/nefos/manifests/init.pp
+++ puppet/modules/nefos/manifests/init.pp
@@ -2,4 +2,23 @@ class nefos {
   # Require an apt-get update for all package declarations
   exec { 'apt-get update': path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' }
   Exec['apt-get update'] -> Package <| |>
+
+  # Install common packages to all servers
+  if ! defined(Package['dnsutils'])        { package { 'dnsutils':        ensure => present } }
+  if ! defined(Package['screen'])          { package { 'screen':          ensure => present } }
+  if ! defined(Package['less'])            { package { 'less':            ensure => present } }
+  if ! defined(Package['curl'])            { package { 'curl':            ensure => present } }
+  if ! defined(Package['wget'])            { package { 'wget':            ensure => present } }
+  if ! defined(Package['bash-completion']) { package { 'bash-completion': ensure => present } }
+  if ! defined(Package['bash'])            { package { 'bash':            ensure => present } }
+  if ! defined(Package['atop'])            { package { 'atop':            ensure => present } }
+  if ! defined(Package['htop'])            { package { 'htop':            ensure => present } }
+  if ! defined(Package['iftop'])           { package { 'iftop':           ensure => present } }
+  if ! defined(Package['lsof'])            { package { 'lsof':            ensure => present } }
+  if ! defined(Package['vim'])             { package { 'vim':             ensure => present } }
+  if ! defined(Package['git'])             { package { 'git':             ensure => present } }
+  if ! defined(Package['git-core'])        { package { 'git-core':        ensure => present } }
+  if ! defined(Package['patch'])           { package { 'patch':           ensure => present } }
+  if ! defined(Package['bzip2'])           { package { 'bzip2':           ensure => present } }
+  if ! defined(Package['sed'])             { package { 'sed':             ensure => present } }
 }
